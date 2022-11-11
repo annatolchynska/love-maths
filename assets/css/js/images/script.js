f75239c7cec0485f37d56b3b1e885809de516aeb
annatolchynska <anna.t1417@gmail.com>
2022-11-11 14:09:20 +0000

    Add final tidy ups for the user.

--- assets/css/js/images/script.js
+++ assets/css/js/images/script.js
@@ -11,6 +11,9 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     }
+    document.getElementById("answer-box").addEventListener('keydown', function(event) {
+        if (event.key === "Enter") {checkAnswer();}
+    })
     runGame("addition");
 });
 
@@ -19,6 +22,8 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed 
  */
 function runGame(gameType) {
+    document.getElementById('answer-box').value = "";
+    document.getElementById('answer-box').focus();
 
     //Creates two randoms between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
