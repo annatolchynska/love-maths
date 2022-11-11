72deaa0d122386d66aadf0f28552672e5c79e8fb
annatolchynska <anna.t1417@gmail.com>
2022-11-11 11:43:03 +0000

    Set up structure of game functions.

---
+++ assets/css/js/images/script.js
@@ -0,0 +1,24 @@
+function runGame() {
+
+}
+function checrAnswer() {
+
+}
+function calculateCorrectAnswer() {
+
+}
+function incrementScore() {
+
+}
+function incrementWrongAnswer() {
+
+}
+function displayAdditionQuestion() {
+
+}
+function displaySubstractQuestion() {
+
+}
+function displayMultiplyQuestion() {
+    
+}
